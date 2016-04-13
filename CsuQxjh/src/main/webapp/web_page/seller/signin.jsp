314074ba5f353a997937a5f9d5c31fb9166aa103
martSforever <marts_forever@163.com>
2016-04-13 16:39:26 +0800

    刚刚没有push所有的资源

---
+++ CsuQxjh/src/main/webapp/web_page/seller/signin.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html class="login-bg">
+<head>
+    <title>情系江华卖家控制台</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="${pageContext.request.contextPath}/web_page/seller/css/bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="${pageContext.request.contextPath}/web_page/seller/css/bootstrap/bootstrap-responsive.css" rel="stylesheet" />
+    <link href="${pageContext.request.contextPath}/web_page/seller/css/bootstrap/bootstrap-overrides.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/web_page/seller/css/layout.css" />
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/web_page/seller/css/elements.css" />
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/web_page/seller/css/icons.css" />
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/web_page/seller/css/lib/font-awesome.css" />
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/web_page/seller/css/compiled/signup.css" type="text/css" media="screen" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+<body>
+    <div class="header">
+        <a href="index.html">
+            <img src="${pageContext.request.contextPath}/web_page/seller/img/logo.png" class="logo" />
+        </a>
+    </div>
+    <div class="row-fluid login-wrapper">
+        <div class="box">
+            <div class="content-wrap">
+                <h6>登录</h6>
+                <input class="span12" type="text" placeholder="用户名" />
+                <input class="span12" type="password" placeholder="密码" />
+                <div class="action">
+                    <a class="btn-glow primary signup" href="index.html">登陆</a>
+                </div>
+        </div>
+    </div>
+
+	<!-- scripts -->
+    <script src="${pageContext.request.contextPath}/web_page/seller/js/jquery-latest.js"></script>
+    <script src="${pageContext.request.contextPath}/web_page/seller/js/bootstrap.min.js"></script>
+    <script src="${pageContext.request.contextPath}/web_page/seller/js/theme.js"></script>
+
+</body>
+</html>
---
+++ CsuQxjh/src/test/java/test/csu/qxjh/admin/TestSellorService.java
@@ -0,0 +1,74 @@
+package test.csu.qxjh.admin;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.csu.qxjh.sellor.dao.SellorDao;
+import com.csu.qxjh.sellor.pojo.Sellor;
+import com.csu.qxjh.sellor.service.SellorService;
+import com.csu.qxjh.user.pojo.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-config.xml" })
+@Transactional
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+public class TestSellorService {
+
+	@Autowired
+	SellorDao sellorDao;
+
+	@Autowired
+	SellorService sellorService;
+	
+	@Test
+	public void testInsert() {
+		/*
+		 * Sellor sellor = new Sellor();
+		 * 
+		 * sellor.setSellor_name("蜘蛛侠"); sellor.setSellor_real_name(
+		 * "Helawdafalo man"); sellor.setSellor_nickname("susdafaw hero");
+		 * sellor.setSellor_login_name("lisi"); sellor.setSellor_sex(0);
+		 * 
+		 * sellorDao.insert(sellor); System.out.println(sellor.getId());
+		 */
+
+		for (int i = 1; i < +20; i++) {
+			Sellor sellor = new Sellor();
+			sellor.setSellor_name("卖家name" + i);
+			sellor.setSellor_login_name("卖家login name" + i);
+			sellor.setSellor_nickname("卖家nick name" + i);
+			sellor.setSellor_real_name("卖家real name" + i);
+			sellorDao.insert(sellor);
+		}
+	}
+
+	@Test
+	public void testGet() {
+		Sellor sellor = sellorDao.selectById("297e8373540e65fe01540e6607420000");
+		System.out.println(sellor.getSellor_login_name());
+	}
+	
+	@Test
+	public void testFuzzyPageQuery(){
+		Map<String, Object> map = sellorService.fuzzyPageQuery(3, null);
+		List<Sellor> sellors = (List<Sellor>) map.get("sellors");
+		for(Sellor sellor:sellors){
+			System.out.println(sellor.getSellor_login_name());
+		}
+		Map<String, Integer> startAndEnd = (Map<String, Integer>) map.get("startAndEnd");
+		System.err.println("start->"+startAndEnd.get("start"));
+		System.err.println("end->"+startAndEnd.get("end"));
+		System.out.println("pageIndex->"+map.get("pageIndex"));
+		System.out.println("pageCounts->"+map.get("pageCounts"));
+		System.out.println("key->"+map.get("key"));
+	}
+
+}
